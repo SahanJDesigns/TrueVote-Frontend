aaea45363b14d874a036a4ec6090ac879fc5de0c
Jegatheesan Kavienan <kavienanj@gmail.com>
2025-05-07 22:22:15 +0530

    Update FACTORY_ADDRESS in constants.ts to new deployed address

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,4 +4,4 @@ import CampaignFactoryABI from './abis/CampaignFactory.json';
 export const CAMPAIGN_ABI = CampaignABI.abi;
 export const FACTORY_ABI = CampaignFactoryABI.abi;
 
-export const FACTORY_ADDRESS = "0x2486CFa41a7a2ab747Ee408716C73cc00e95a31f"; // ← paste from deploy
+export const FACTORY_ADDRESS = "0xCD66ee88188FAC264d9444Fd4069032AD8B18e2D";
